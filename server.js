9683e788fff0ba7a4f6d89e8e5983c6ebe8048a2
freejacque <freejacque@gmail.com>
2015-09-13 14:30:15 -0400

    added notes to server.js

--- server.js
+++ server.js
@@ -2,9 +2,14 @@
 var http = require("http");
 
 // createServer is a function in the http module that returns an object
+// pass an anonymous function to createServer function
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
   // listen is a method of the object created by createServer
 }).listen(8888);
+
+  // alternate server code
+// var server = http.createServer();
+// server.listen(8888);
