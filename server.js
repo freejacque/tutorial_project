331b6a399194d96da32ce22409f522cb5788e094
freejacque <freejacque@gmail.com>
2015-09-13 14:22:17 -0400

    added notes to server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+// require the http module and set it to a variable
 var http = require("http");
 
+// createServer is a function in the http module that returns an object
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response,write("Hello World");
+  response.write("Hello World");
   response.end();
+  // listen is a method of the object created by createServer
 }).listen(8888);
