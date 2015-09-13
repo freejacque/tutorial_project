9e2d3ff97fa9bb4263b5f0a4b6d76165cfe16930
freejacque <freejacque@gmail.com>
2015-09-13 15:30:30 -0400

    added route parameter to start function

--- server.js
+++ server.js
@@ -1,29 +1,22 @@
 // require the http module and set it to a local variable
 // convention is to make the module name as the variable name
 var http = require("http");
+// require url module
 var url = require("url");
+
 // createServer is a function in the http module that returns an object
-// pass an anonymous function to createServer function
-  // http.createServer(function(request, response) {
-  //   response.writeHead(200, {"Content-Type": "text/plain"});
-  //   response.write("Hello World");
-  //   response.end();
-  //   // listen is a public method of the object created by createServer
-  // }).listen(8888);
-
-  // alternate server code
-// var server = http.createServer();
-// server.listen(8888);
-
-function start() {
+function start(route) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log("request received.");
+    console.log("Request for " + pathname + " received.");
+
+    route(pathname);
+
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("***Kanye Shrug***");
     response.end();
   }
-
+// listen() is a public funtion in the http module
   http.createServer(onRequest).listen(8888);
   console.log("Server has started");
 }
