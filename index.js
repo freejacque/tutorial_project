a45926e07c47387ce9f9c8a53466264f43f9de1a
freejacque <freejacque@gmail.com>
2015-09-14 19:57:18 -0400

    changed file name, created a handle object to handle request

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var server = require("./server");
 var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+
 
 // call the server function and pass in the router function "route"
 server.start(router.route);
