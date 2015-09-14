08e118809d806afdb589e7b414a041c433afab88
freejacque <freejacque@gmail.com>
2015-09-14 19:59:51 -0400

    added notes

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
-
+// create a handle object to handle requests
+var handle = {};
+// the handlers are key/value pairs
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
 
 // call the server function and pass in the router function "route"
 server.start(router.route);
