4cd4309f8bb306ae666dc575e8ddfa32b69dc252
freejacque <freejacque@gmail.com>
2015-09-15 20:23:34 -0400

    added comments

--- router.js
+++ router.js
@@ -1,6 +1,8 @@
 function route(handle, pathname) {
   console.log("About to route a request for " + pathname);
+  // if there is a handler for a given pathname...
   if (typeof handle[pathname] === 'function') {
+    // we call that function
     handle[pathname]();
   } else {
     console.log("No request handler found for " + pathname);
