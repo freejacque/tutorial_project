36da3d78d22f9bcc6937af4beb45f29dd09546d1
freejacque <freejacque@gmail.com>
2015-09-24 19:28:03 -0400

    deleted variable content

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,7 +3,6 @@ var exec = require("child_process").exec;
 
 function start(response) {
   console.log("Request handler 'start' was called.");
-  var content = "empty";
 
   exec("ls -lah", function (error, stdout, stderr){
     content = stdout;
