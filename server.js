1e8f374fd01ace50aba58c2091bb44460e768e63
freejacque <freejacque@gmail.com>
2015-09-25 21:11:07 -0400

    added comments

--- server.js
+++ server.js
@@ -12,14 +12,17 @@ function start(route, handle) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
+    // defined the expected encoding of the received data
     request.setEncoding("utf8");
 
+    //  data is received in chunks and concatenated to form postData
     request.addListener("data", function(postDataChunk){
       postData += postDataChunk;
       console.log("Received POST data chunk '" +
         postDataChunk + "'.");
     });
 
+    // once all data has been gathered, the router is called and passed the data
     request.addListener("end", function() {
       route(handle, pathname, response, postData);
     });
