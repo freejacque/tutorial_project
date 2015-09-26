a1d4b724916d0d2d16a405118a5281d2b05b0573
freejacque <freejacque@gmail.com>
2015-09-25 21:16:48 -0400

    added postData param to route()

--- router.js
+++ router.js
@@ -1,9 +1,9 @@
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) {
   console.log("About to route a request for " + pathname);
   // if there is a handler for a given pathname...
   if (typeof handle[pathname] === 'function') {
     // we call that function
-    handle[pathname](response);
+    handle[pathname](response, postData);
   } else {
     console.log("No request handler found for " + pathname);
     response.writeHead(404, {"Content-Type": "text/plain"});
