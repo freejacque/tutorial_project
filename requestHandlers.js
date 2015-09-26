a973278fe297ac093381da976bad56d1b9c358be
freejacque <freejacque@gmail.com>
2015-09-25 21:24:00 -0400

    added a querystring parse of text

--- requestHandlers.js
+++ requestHandlers.js
@@ -25,7 +25,7 @@ function start(response, postData) {
 function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent: " + postData);
+  response.write("You've sent: " + querystring.parse(postData).text);
   response.end();
 }
 
