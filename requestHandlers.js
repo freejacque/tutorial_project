84444fdd9dd16a78d7155ae3d4093a15c31e9047
freejacque <freejacque@gmail.com>
2015-09-25 20:41:23 -0400

    removed exec()

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,3 @@
-var exec = require("child_process").exec;
-
-
 function start(response) {
   console.log("Request handler 'start' was called.");
 
@@ -16,13 +13,10 @@ function start(response) {
     '</form>' +
     '</body>' +
     '</html>';
-  // exec("ls -lah",
-    exec("find /", { timeout: 10000, maxBuffer: 20000*1024},
-    function (error, stdout, stderr){
+
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write(stdout);
     response.end();
-  });
 }
 
 function upload(response) {
