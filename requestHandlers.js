955b5d693f6352c5246dcf04eff941880a130f03
freejacque <freejacque@gmail.com>
2015-09-25 21:19:47 -0400

    added postData param to start() and upload()

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,4 @@
-function start(response) {
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
   // text area to be filled user and submitted in a post request
@@ -20,10 +20,10 @@ function start(response) {
     response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent: " + postData);
   response.end();
 }
 
