0ba9c0f405f4faf54d1b14baf3519a5f3e178aec
freejacque <freejacque@gmail.com>
2015-09-27 21:31:37 -0400

    created  show()

--- requestHandlers.js
+++ requestHandlers.js
@@ -30,5 +30,11 @@ function upload(response, postData) {
   response.end();
 }
 
+function show(response) {
+  console.log("Request handler 'show' was called.");
+  response.writeHead(200, {"Content-Type: "image/png"});
+  fs.createReadStream("/tmp/test.png).pipe(response);
+}
+
 exports.start = start;
 exports.upload = upload;
