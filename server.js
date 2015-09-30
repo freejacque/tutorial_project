a86a1c64867d97cb4b8d0d33a8b1b69def90df85
freejacque <freejacque@gmail.com>
2015-09-29 21:33:10 -0400

    refactored server.js to remove postData

--- server.js
+++ server.js
@@ -8,25 +8,9 @@ var url = require("url");
 // the start function is being passed the route and the handle object
 function start(route, handle) {
   function onRequest(request, response) {
-    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
-
-    // defined the expected encoding of the received data
-    request.setEncoding("utf8");
-
-    //  data is received in chunks and concatenated to form postData
-    request.addListener("data", function(postDataChunk){
-      postData += postDataChunk;
-      console.log("Received POST data chunk '" +
-        postDataChunk + "'.");
-    });
-
-    // once all data has been gathered, the router is called and passed the data
-    request.addListener("end", function() {
-      route(handle, pathname, response, postData);
-    });
-
+    route(handle, pathname, response, request);
   }
 
 // listen() is a public funtion in the http module
